e25bcaa6815a2a9825b5c9982c6a391d877bd513
mrchiraggarg <gargchirag728@gmail.com>
2025-04-10 22:21:27 +0530

    commit

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { phrases, Phrase } from "./data";
 import GibberishCard from "./GibberishCard";
 import Result from "./Result";
